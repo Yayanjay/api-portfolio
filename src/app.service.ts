75c83d928d81186d4b5ed2d6d7e49c21cfe5306d
Yayanjay <yayanabdillah01@gmail.com>
2023-03-28 11:51:12 +0700

    fixing upload

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { S3 } from 'aws-sdk';
-import { readFileSync } from 'fs'
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class AppService {
-  S3_BUCKET = process.env.AWS_S3_BUCKET;
+  S3_BUCKET = process.env.ICH_S3_BUCKET;
   s3 = new S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    endpoint: process.env.AWS_ENDPOINT
+    accessKeyId: process.env.ICH_ACCESS_KEY_ID,
+    secretAccessKey: process.env.ICH_SECRET_ACCESS_KEY,
+    endpoint: process.env.ICH_ENDPOINT,
   });
 
   async uploadFile(file) {
+    console.log(file)
     const { originalname } = file;
 
     await this.s3_upload(
@@ -29,17 +30,14 @@ export class AppService {
       Body: file,
       ACL: 'public-read',
       ContentType: mimetype,
-      ContentDisposition: 'inline',
-      // CreateBucketConfiguration: {
-      //   LocationConstraint: 'EastJKT-a',
-      // },
+      ContentDisposition: 'inline'
     };
     console.log(params);
 
     try {
       const s3Response = await this.s3.upload(params).promise();
       console.log(s3Response);
-      return s3Response
+      return s3Response;
     } catch (e) {
       console.log(e);
     }
