18afb755aba747b66f29b2198b281cb6e421e82b
Yayanjay <yayanabdillah01@gmail.com>
2023-03-28 11:50:55 +0700

    remove aws-sdk warning log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('aws-sdk/lib/maintenance_mode_message').suppress = true;
+
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -9,27 +12,26 @@ import { ProjectRepo } from './projects/entities/project-repo.entity';
 import { ProjectImage } from './projects/entities/project-images.entity';
 import { MulterModule } from '@nestjs/platform-express';
 import { ConfigModule } from '@nestjs/config';
-
 @Module({
   imports: [
     ProjectsModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.developmet'],
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'yayanjay123',
-      password: 'yayanjay123',
-      database: 'portfolio_db',
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Project, ProjectTag, ProjectRepo, ProjectImage],
       synchronize: true,
     }),
     MulterModule.register({
       dest: '../public/upload',
     }),
-    ConfigModule.forRoot({
-      envFilePath: ['.env', '.env.developmet'],
-      isGlobal: true,
-    })
   ],
   controllers: [AppController],
   providers: [AppService],
